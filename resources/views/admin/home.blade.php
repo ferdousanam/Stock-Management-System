fff39fa8e4c4523e641118f787075f77092269cc
Ferdous Anam <ferdous.anam@gmail.com>
2021-01-20 21:58:40 +0600

    Dashboard Product List Url

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -12,13 +12,12 @@
             <div class="small-box bg-info">
                 <div class="inner">
                     <h3>{{ $totalProducts }}</h3>
-
                     <p>Products</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('admin.products.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
