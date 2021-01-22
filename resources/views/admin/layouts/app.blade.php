ed6c1c3c7ff7d63462eee3f7ebac5b501469701d
Ferdous Anam <ferdous.anam@gmail.com>
2021-01-23 01:21:16 +0600

    favicon updated

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <title>{{ config('app.name', 'Laravel') }} || {{isset($pageTitle) ? $pageTitle :''}}</title>
 
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}"/>
+
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Ionicons -->
