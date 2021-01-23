13ef4f00e727b71ff116d1ae0e108556f3d851f9
Ferdous Anam <ferdous.anam@gmail.com>
2021-01-24 00:25:51 +0600

    product autocomplete search function

---
+++ resources/js/admin/autocomplete/product.js
@@ -0,0 +1,58 @@
+window.productSuggestions = function (callback) {
+    return {
+        source: function (request, response) {
+            $.ajax({
+                type: 'get',
+                url: window.base_url + '/api/products/suggestions',
+                dataType: "json",
+                data: {
+                    q: request.term
+                },
+                success: function (data) {
+                    $(this).removeClass('ui-autocomplete-loading');
+                    response($.map(data.data, function (item) {
+                        return {
+                            ...item,
+                            label: `${item.title} (${item.product_code})`,
+                            value: item.id
+                        };
+                    }));
+                }
+            });
+        },
+        minLength: 1,
+        autoFocus: false,
+        delay: 250,
+        response: function (event, ui) {
+            if ($(this).val().length >= 16 && ui.content[0].id === 0) {
+                //audio_error.play();
+                bootbox.alert('No matching result found! Product might be out of stock in the selected warehouse.', function () {
+                    $('#add_item').focus();
+                });
+                $(this).removeClass('ui-autocomplete-loading');
+                $(this).val('');
+            } else if (ui.content.length === 1 && ui.content[0].id !== 0) {
+                ui.item = ui.content[0];
+                $(this).data('ui-autocomplete')._trigger('select', 'autocompleteselect', ui);
+                $(this).autocomplete('close');
+                $(this).removeClass('ui-autocomplete-loading');
+            } else if (ui.content.length === 1 && ui.content[0].id === 0) {
+                //audio_error.play();
+                bootbox.alert('No matching result found! Product might be out of stock in the selected warehouse.', function () {
+                    $('#add_item').focus();
+                });
+                $(this).removeClass('ui-autocomplete-loading');
+                $(this).val('');
+            }
+        },
+        select: function (event, ui) {
+            event.preventDefault();
+            if (ui.item.id !== 0) {
+                callback(ui.item);
+            } else {
+                //audio_error.play();
+                bootbox.alert('No matching result found! Product might be out of stock in the selected warehouse.');
+            }
+        }
+    };
+}
