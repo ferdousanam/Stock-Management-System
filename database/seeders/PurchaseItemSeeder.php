f34920af81cd3b3915cae1b9b521471ab3ec8583
Ferdous Anam <ferdous.anam@gmail.com>
2021-01-30 16:26:19 +0600

    Updated PurchaseItemSeeder

--- database/seeders/PurchaseItemSeeder.php
+++ database/seeders/PurchaseItemSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PurchaseItemSeeder extends Seeder
 {
@@ -14,5 +15,7 @@ class PurchaseItemSeeder extends Seeder
     public function run()
     {
         \App\Models\PurchaseItem::factory(500)->create();
+
+        DB::statement("UPDATE purchases SET purchases.net_total = IFNULL((SELECT SUM(purchase_items.net_cost) FROM purchase_items WHERE purchase_items.purchase_id = purchases.id GROUP BY purchase_id), 0)");
     }
 }
